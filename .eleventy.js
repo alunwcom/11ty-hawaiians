79430e2c9e13e4b16ab5168f9fcef28eded1d48d
Alun 'Will' Williams <alun@alunw.com>
2023-02-19 12:13:58 +0000

    Initial Photoswipe implementation

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,12 @@ const bandcamp = require('./src/_shortcodes/Bandcamp');
 module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy({'src/_static':'.'});
+  eleventyConfig.addPassthroughCopy({
+    "node_modules/photoswipe/dist/photoswipe-lightbox.esm.min.js": "js/photoswipe-lightbox.esm.min.js",
+    "node_modules/photoswipe/dist/photoswipe.esm.min.js": "js/photoswipe.esm.min.js",
+    "node_modules/photoswipe/dist/photoswipe.css": "css/photoswipe.css"
+  });
+  
   
   eleventyConfig.addShortcode('DiscoImage', discoImage);
   eleventyConfig.addShortcode('FacebookVideo', facebookVideo);
@@ -15,6 +21,7 @@ module.exports = function(eleventyConfig) {
       input: 'src',
       includes: '_includes',
       layouts: '_layouts',
+      data: '_data',
       output: '_site'
     }
   }
